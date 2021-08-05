cbdc787be8e397491aed1697004cd26cda7692cb
Venk <vkunduru01@gmail.com>
2021-08-05 14:09:45 -0400

    made some changes

--- Phase1/Simply Blogging/createBlogs.js
+++ Phase1/Simply Blogging/createBlogs.js
@@ -9,9 +9,12 @@ function addBlog(){
   var reader = new FileReader();
 
   const blogpost2 = document.createElement("div");
+  blogpost2.style.backgroundColor= "teal";
+  blogpost2.style.color= "white";
+  blogpost2.style.borderRadius = 10 + "px";
   blogpost2.style.margin = 15+"px";
-  blogpost2.style.width = 300+"px";
-  blogpost2.style.height = 300+"px";
+  blogpost2.style.width = 400+"px";
+  blogpost2.style.height = 400+"px";
   blogpost2.style.border = 1+"px solid black";
   blogpost2.style.wordWrap = "break-word";
   blogpost2.style.overflowY = "auto";
