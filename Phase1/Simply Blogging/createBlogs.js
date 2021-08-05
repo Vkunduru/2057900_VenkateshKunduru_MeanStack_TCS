9d25ec06921843934aa03c4257857dacda39b049
Venk <vkunduru01@gmail.com>
2021-08-05 12:31:57 -0400

    made some changes

--- Phase1/Simply Blogging/createBlogs.js
+++ Phase1/Simply Blogging/createBlogs.js
@@ -10,8 +10,11 @@ function addBlog(){
 
   const blogpost2 = document.createElement("div");
   blogpost2.style.margin = 15+"px";
-  blogpost2.style.width = 200+"px";
+  blogpost2.style.width = 400+"px";
+  blogpost2.style.height = 400+"px";
   blogpost2.style.border = 1+"px solid black";
+  blogpost2.style.overflow = "auto";
+
   blogpost2.innerHTML = "<image src=" + blogpost.image + " width=200 height=200> <br> <h3>" + blogpost.title + "</h3> <br> <p>" + blogpost.text + "</p>";
 
   document.getElementById("blogspace").appendChild(blogpost2);
