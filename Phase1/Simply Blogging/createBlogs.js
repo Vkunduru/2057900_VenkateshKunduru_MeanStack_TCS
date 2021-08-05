6fa70d4fc8f2a019f7affe1fe160469de63a72dd
Venk <vkunduru01@gmail.com>
2021-08-05 13:52:45 -0400

    made some changes

--- Phase1/Simply Blogging/createBlogs.js
+++ Phase1/Simply Blogging/createBlogs.js
@@ -10,12 +10,13 @@ function addBlog(){
 
   const blogpost2 = document.createElement("div");
   blogpost2.style.margin = 15+"px";
-  blogpost2.style.width = 400+"px";
-  blogpost2.style.height = 400+"px";
+  blogpost2.style.width = 300+"px";
+  blogpost2.style.height = 300+"px";
   blogpost2.style.border = 1+"px solid black";
-  blogpost2.style.overflow = "auto";
+  blogpost2.style.wordWrap = "break-word";
+  blogpost2.style.overflowY = "auto";
 
-  blogpost2.innerHTML = "<image src=" + blogpost.image + " width=200 height=200> <br> <h3>" + blogpost.title + "</h3> <br> <p>" + blogpost.text + "</p>";
+  blogpost2.innerHTML = "<image src=" + blogpost.image + " width=200 height=200 style = 'margin:auto; display: block;'> <br> <h3 style='text-align:center;'>" + blogpost.title + "</h3> <br> <p>" + blogpost.text + "</p>";
 
   document.getElementById("blogspace").appendChild(blogpost2);
 
